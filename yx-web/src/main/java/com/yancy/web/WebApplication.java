ede2b6a2b9f73bb2800afa2f565a637ee2fbf0b0
yancyyao@hotmail.com <Jrcchzxsl123>
2018-06-12 16:58:53 +0800

    @add web

---
+++ yx-web/src/main/java/com/yancy/web/WebApplication.java
@@ -0,0 +1,19 @@
+package com.yancy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Web启动类
+ *
+ * @author:yaoxin
+ * @date:Created in 2018/5/31 15:27
+ * @description:
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
